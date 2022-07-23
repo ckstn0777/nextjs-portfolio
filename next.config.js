c8488da153e86dda2bc73e87bd2305183e1851b6
ckstn0777 <ckstn0777@naver.com>
2022-07-23 23:16:20 +0900

    update next.config.js 및 배포 스크립트 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== "production";
+const name = "nextjs-portfolio";
+
 const nextConfig = {
+  basePath: !debug ? `/${name}` : "",
+  images: {
+    loader: "akamai",
+    path: "",
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
